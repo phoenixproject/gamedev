7948e563d510294786e3630740acc7d98cf65801
phoenixproject.erp@gmail.com <phoenixproject.erp@gmail.com>
2018-12-24 19:26:16 -0200

    Snake Corrigido e Finalizado

--- _HTML/_SNAKE/js/snake.js
+++ _HTML/_SNAKE/js/snake.js
@@ -16,7 +16,7 @@ var pontos = 0;
 var food = {
     x : Math.floor(Math.random()*29+1) * box,
     y : Math.floor(Math.random()*29+1) * box
-}
+};
 
 var d;
 
@@ -25,20 +25,20 @@ document.addEventListener("keydown",direcao);
 function direcao(event){
     var key = event.keyCode;
     
-    if(key == 37 && d != "RIGHT"){
+    if(key === 37 && d !== "RIGHT"){
         d = "LEFT";
-    } else if(key == 38 && d != "DOWN"){
+    } else if(key === 38 && d !== "DOWN"){
         d = "UP";
-    } else if(key == 39 && d != "LEFT"){
+    } else if(key === 39 && d !== "LEFT"){
         d = "RIGHT";
-    } else if(key == 40 && d != "UP"){
+    } else if(key === 40 && d !== "UP"){
         d = "DOWN";
     }
 }
 
 function collision(head,array){
     for(var i = 0; i < array.length; i++){
-        if(head.x == array[i].x && head.y == array[i].y){
+        if(head.x === array[i].x && head.y === array[i].y){
             return true;
         }
     }
@@ -50,7 +50,7 @@ function draw(){
     ctx.fillRect(0,20,cvs.width,cvs.height);
     
     for(i=0; i < snake.length; i++){
-        ctx.fillStyle = ( i == 0)? "green" : "blue";
+        ctx.fillStyle = ( i === 0)? "green" : "blue";
         ctx.fillRect(snake[i].x, snake[i].y,box, box);
         ctx.strokeStyle = "red";
         ctx.strokeRect(snake[i].x,snake[i].y,box,box);
@@ -62,16 +62,16 @@ function draw(){
     var snakeX = snake[0].x;
     var snakeY = snake[0].y;
     
-    if( d == "LEFT") snakeX -= box;
-    if( d == "UP") snakeY -= box;
-    if( d == "RIGHT") snakeX += box;
-    if( d == "DOWN") snakeY += box;
+    if( d === "LEFT")  snakeX -= box;
+    if( d === "UP")    snakeY -= box;
+    if( d === "RIGHT") snakeX += box;
+    if( d === "DOWN")  snakeY += box;
     
-    if(snakeX == food.x && snakeY == food.y){
+    if(snakeX === food.x && snakeY === food.y){
         food = {
             x : Math.floor(Math.random()*29*1) * box,
             y : Math.floor(Math.random()*29*1) * box
-        }
+        };
         somaPonto();
     }
     else{
@@ -81,7 +81,7 @@ function draw(){
     var newHead = {
         x : snakeX,
         y : snakeY
-    }
+    };
     
     if(snakeX < 0 || snakeX > cvs.width-box || snakeY < 20 || snakeY > cvs.height-box || collision(newHead,snake)){
         clearInterval(game);
@@ -92,39 +92,39 @@ function draw(){
 }
 
 function somaPonto(){
-    pontos == pontos + 1;
+    pontos = pontos + 1;
     mostrarPontos();
 }
 
 function gravaRecorde(){
     var record = 0;
-    if (localStorage.getItem("pontuacaoSnake") != null){
-        record = parseINt(localStorage.getItem("pontuacaoSnake"));
+    if (localStorage.getItem("pontuacaoSnake") !== null){
+        record = parseInt(localStorage.getItem("pontuacaoSnake"));
         if(record < pontos){
             record = pontos;
         }
-        else{
-            record = pontos;
-        }
-        localStorage.setItem("pontuacaoSnake",record);
-        mostrarRecorde();
+    }else{
+        record = pontos;
     }
+    localStorage.setItem("pontuacaoSnake",record);
+    mostrarRecorde();    
 }
 
 function mostrarRecorde(){
     ctx.clearRect(cvs.width - 250,0,cvs.width,20);
     var record = 0;
-    ctx.fillStyle = "20px Arial";
-    if(localStorage.getItem("pontuacaoSnake") != null){
+    ctx.fillStyle = "0000ff";
+    ctx.font = "20px Arial";
+    if(localStorage.getItem("pontuacaoSnake") !== null){
         record = parseInt(localStorage.getItem("pontuacaoSnake"));
     }
-    ctx.fillText("Recorde: "+record, cvs.width - 250,16,250);
+    ctx.fillText("Recorde: " + record, cvs.width - 250,16,250);
 }
 
 function mostrarPontos(){
     ctx.clearRect(0,0,250,20);
     ctx.fillStyle = "#ff0000";
-    ctx.fillText("Pontos: "+pontos, 1,16,250);
+    ctx.fillText("Pontos: " + pontos, 1,16,250);
 }
 
 mostrarRecorde();
