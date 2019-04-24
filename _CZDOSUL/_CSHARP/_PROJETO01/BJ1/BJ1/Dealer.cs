163c0055be2b091f33bfc33a1721ac33b2cfcdaf
Phoenix Project <phoenixproject.erp@gmail.com>
2019-04-23 23:35:26 -0300

    Projeto C# adicionado

---
+++ _CZDOSUL/_CSHARP/_PROJETO01/BJ1/BJ1/Dealer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BJ1
+{
+    class Dealer  : Jogador
+    {
+        public Dealer()
+            :base("Dealer")
+        {
+            
+        }
+
+        public override bool QuerCarta()
+        {
+            return ValorMao() < 17;
+        }
+    }
+}
