f64083310cfa820cd34d3e5cff7078b5103010ff
Phoenix Project <phoenixproject.erp@gmail.com>
2019-11-17 14:28:02 -0300

    10 - Scrolling the Background - finalizado

--- _UDEMY/_UNITY/_SPACESHOOT/Assets/Scripts/BackgroundScroller.cs
+++ _UDEMY/_UNITY/_SPACESHOOT/Assets/Scripts/BackgroundScroller.cs
@@ -4,20 +4,28 @@ using UnityEngine;
 
 public class BackgroundScroller : MonoBehaviour
 {
+    // Atributos responsáveis pelos backgrounds do game
     public Transform BG1, BG2;
+
+    // Atributo responsável pela velocidade que o background anda
     public float scrollSpeed;
 
+    // Atributo responsável por obter a largura de um background
     private float bgWidth;
 
     // Start is called before the first frame update
     void Start()
     {
+        // Para obter a largura do primeiro background tem 
+        // que obter o tamanho dos limites do eixo X do sprite contido
+        // no componente SpriteRenderer
         bgWidth = BG1.GetComponent<SpriteRenderer>().sprite.bounds.size.x;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //
         BG1.position = new Vector3(BG1.position.x - (scrollSpeed * Time.deltaTime), BG1.position.y, BG2.position.z);
         BG2.position -= new Vector3(scrollSpeed * Time.deltaTime, 0f, 0f);
 
