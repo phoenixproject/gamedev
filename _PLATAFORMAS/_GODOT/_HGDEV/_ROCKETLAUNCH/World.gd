667215479f23a1474a258a71c826aa159545cda1
Phoenix Project <phoenixproject.erp@gmail.com>
2021-05-16 02:25:32 -0300

    Aula 6 Godot concluída

--- _PLATAFORMAS/_GODOT/_HGDEV/_ROCKETLAUNCH/World.gd
+++ _PLATAFORMAS/_GODOT/_HGDEV/_ROCKETLAUNCH/World.gd
@@ -3,5 +3,9 @@
 onready var animationPlayer = get_node("AnimationPlayer")
 #onready var animationPlayer = $animationPlayer
 
-func _ready():
+#func _ready():
+#	animationPlayer.play("Launch")
+
+func _on_LaunchButton_pressed():
+	#pass # Replace with function body.
 	animationPlayer.play("Launch")
